965a122956123bc3e9f8c3bbdd74beb78557b0f8
neeru0713 <neerurani1307@gmail.com>
2025-06-25 03:24:20 +0530

    redis url

--- cache.js
+++ cache.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const redis = require("redis");
 const client = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL,
 });
 
 client.on("error", (err) => console.error("Redis error", err));
